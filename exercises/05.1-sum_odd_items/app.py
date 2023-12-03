c00db4e7da842d91b435e78a5a0e9b25a24126bc
Catalina Scheleff Cáceres <122483646+CatalinaScheleff@users.noreply.github.com>
2023-12-03 23:46:40 +0000

    6.1

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,3 +1,13 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
+def sum_odds(numbers):
+    total = 0
+
+    for i in range(0,len(arr)):
+        if numbers[i]%2 != 0:
+            total += numbers[i]
+    return total
+
+print(sum_odds(arr))
+
