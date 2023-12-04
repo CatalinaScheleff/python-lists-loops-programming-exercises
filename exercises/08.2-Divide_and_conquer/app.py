e6fb0783f1516ef9ace8f4e8401f07970cf5fedc
Catalina Scheleff Cáceres <122483646+CatalinaScheleff@users.noreply.github.com>
2023-12-04 00:59:40 +0000

    10

--- exercises/08.2-Divide_and_conquer/app.py
+++ exercises/08.2-Divide_and_conquer/app.py
@@ -2,7 +2,20 @@ list_of_numbers = [4,	80,	85,	59,	37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76,
 
 
 #Your code here:
+def merge_two_list(numbers):
+    odd = []
+    even = []
+    merged = []
 
+    for numb in numbers:
+        if numb%2 == 0:
+            even.append(numb)
+        else:
+            odd.append(numb)
+    
+    return odd + even
+
+    
 
 print(merge_two_list(list_of_numbers))
 
