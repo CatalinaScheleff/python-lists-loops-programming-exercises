6c85813ab724d389bb508ed9ab085aa82a77d382
Catalina Scheleff Cáceres <122483646+CatalinaScheleff@users.noreply.github.com>
2023-12-04 22:18:35 +0000

    10.1

--- exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
+++ exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
@@ -3,5 +3,8 @@ contact = {
     "phone": "321-321-4321",
     "email": "test@test.com"
 }
-#Your code here:
 
+# Recorre las claves del diccionario
+for key in contact:
+    # Imprime la clave y el valor correspondiente
+    print(f"{key} : {contact[key]}")
